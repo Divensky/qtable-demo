4d50c0b515838e4910ec8d5c478072f92bdfc13f
Irina Kats <irina8kats@gmail.com>
2024-07-07 10:15:52 -0400

    feat: add BaseTooltip

---
+++ src/components/base/BaseTooltip.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'BaseTooltip',
+});
+</script>
+
+<template>
+  <q-tooltip class="base-tooltip">
+    <slot></slot>
+  </q-tooltip>
+</template>
+
+<style lang="scss">
+.base-tooltip {
+  padding: 12px;
+  color: var(--q-dark);
+  font-size: 16px;
+  text-align: center;
+  background-color: #fff;
+  border-radius: 16px;
+  box-shadow: 0 4px 20px 0 #00000014;
+
+  @media (max-width: 1450px) {
+    padding: 8px 12px;
+    font-size: 14px;
+  }
+}
+</style>
