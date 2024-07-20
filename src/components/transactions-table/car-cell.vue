4bb94f1e7f02e2d305db589e922ccb4da1a1963e
Irina Kats <irina8kats@gmail.com>
2024-07-20 07:04:13 -0400

    feat: add CarCell

---
+++ src/components/transactions-table/car-cell.vue
@@ -0,0 +1,49 @@
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import { Car } from '@/components/transactions-table/types';
+import { classNames } from '@/ts/classNames';
+
+export default defineComponent({
+  name: 'CarCell',
+  props: {
+    car: {
+      type: Object as PropType<Car>,
+      required: true,
+    },
+  },
+  setup() {
+    return { cn: classNames('car-cell') };
+  },
+});
+</script>
+
+<template>
+  <div :class="cn()">
+    <div
+      v-if="car.vin"
+      :class="cn('nowrap')"
+    >
+      <span :class="cn('text-gray')">VIN</span>
+      {{ car.vin }}
+    </div>
+    <div v-if="car.id">
+      <span :class="cn('text-gray')">ID </span>
+      <span>{{ car.id }}</span>
+    </div>
+    <div v-if="car.model">
+      {{ car.model }}
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.car-cell {
+  &__nowrap {
+    white-space: nowrap;
+  }
+
+  &__text-gray {
+    color: var(--q-dark-page);
+  }
+}
+</style>
