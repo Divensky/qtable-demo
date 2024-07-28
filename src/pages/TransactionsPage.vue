963c173cb7c396cbe322edf84d97523a227cfb12
Irina Kats <irina8kats@gmail.com>
2024-07-27 21:59:27 -0400

    feat: add Transactions Page

---
+++ src/pages/TransactionsPage.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import TransactionsTable from '@/components/transactions-table/TransactionsTable.vue';
+</script>
+
+<template>
+  <TransactionsTable></TransactionsTable>
+</template>
+
+<style scoped lang="scss"></style>
